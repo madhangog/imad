31a4820de9d455d8875d53788c7dcadeca55247c
madhangog <madhanmanoj2017@gmail.com>
2018-08-10 19:22:22 +0530

    changes made

--- server.js
+++ server.js
@@ -8,17 +8,37 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter = 0 ;
+app.get('/counter', function (req, res) {
+	counter = counter +1;
+	res.send("count = "+counter.toString());
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+app.get('/ui/maddy.jpg', function (req, res) {
+ res.sendFile(path.join(__dirname, 'ui', 'maddy.jpg'));
+});
+ var names = [];
+app.get('/submitname', function (req, res) {
+
+	//submitname?name=XXX
+	//get the name from request
+	var name = req.query.name;
+	names.push(name);
+	//JSON : javascript object notatopon
+ 	res.send(JSON.stringify(names));
 });
 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+
+var port = 8080; // Use 8080 for local development[	] because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`MADDY  listening on port ${port}!`);
 });
+ 
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,61 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+button.onclick = function(){
+
+	//creating requesr\t object
+
+	var request = new XMLHttpRequest()
+
+
+		// capture the response iand store it in the variable
+
+
+	request.onreadystatechange = function(){
+		if(request.readyState === XMLHttpRequest.DONE){
+			if(request.status===200){
+				var counter = request.responseText;
+    			var span = document.getElementById('count')
+    			span.innerHTML=counter.toString();
+    		}
+    	} 
+    };
+
+    //make the request
+
+    request.open('GET', "/counter", true);
+	request.send(null);
+	};	
+
+	
+	var submit = document.getElementById("submitbt");
+	submit.onclick = function() {
+
+			// capture the response iand store it in the variable
+
+			var request = new XMLHttpRequest()
+			request.onreadystatechange = function(){
+			if(request.readyState === XMLHttpRequest.DONE){
+			if(request.status===200){
+					//make the request to the server and send the name
+			//capture the list of names and render it as list
+			var names = request.responseText;
+			names = JSON.parse(names);
+			var list ='';
+			for (var i=0; i<names.length; i++)
+			{
+				list += '<li>'+names[i]+'</li>';
+			}
+			var ul = document.getElementById('namelist');
+			ul.innerHTML = list;
+    		}
+    	} 
+    };
+
+    //make the request
+    //sumbit name 
+	var nameinput = document.getElementById('name');
+	var name = nameinput.value;
+    request.open('GET', "/submitname?name="+name, true);
+	request.send(null);
+	
+	};
+  
